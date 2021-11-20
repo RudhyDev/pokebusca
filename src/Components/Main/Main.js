eaf92f9992b20d7ef77d6f55fe75c14084619d16
RudhyDev <rudhy.pereira@gmail.com>
2021-11-20 14:11:35 -0300

    .

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import './Main.css'
+
+export default function Main(){
+
+    return(
+        <div className="bg">
+            <div className="pokemon-area">
+                
+            </div>
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+import Main from "./Components/Main/Main";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode> 
+        <Main/>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
